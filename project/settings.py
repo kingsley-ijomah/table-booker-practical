84bfff8b95fb1ce749f9cddcae73c1d3830b03e3
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-06-12 18:48:58 +0100

    adding models and admin

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "table_booker.apps.TableBookerConfig",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": str(BASE_DIR / "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "table_booker_practical",
+        "USER": "table_booker_user",
+        "PASSWORD": "r4$AMGexn9a#4mJd",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
