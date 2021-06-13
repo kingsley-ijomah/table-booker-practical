494994964440b10dd47b4169a213e1af3369976c
kingsley ijomah <kingsley.ijomah@gmail.com>
2021-06-13 09:13:26 +0100

    fix typo

--- table_booker/views.py
+++ table_booker/views.py
@@ -45,7 +45,7 @@ def signup_page(request):
             messages.success(request, "Registration successful.")
             return redirect("table_booker:home")
         messages.error(request, "Unsuccessful registration. Invalid information.")
-    form = UserForm
+    form = UserForm()
     return render(
         request=request, template_name="signup.html", context={"register_form": form},
     )
@@ -75,7 +75,7 @@ def book_restaurant(request, restaurant_id):
             messages.info(request, f"You successfully booked {restaurant.name}.")
             return redirect("table_booker:home")
     else:
-        form = BookingForm
+        form = BookingForm()
 
     return render(
         request=request,
